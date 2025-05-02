6da93fadb540f118a8a365086fcfed684d249ecb
vnray <ervnray@gmail.com>
2025-05-02 17:56:09 +0530

    layout changed

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -32,16 +32,20 @@ const CityList = ({ data }) => {
                 {cities.map((city, index) => (
                  
                   <div key={city.id} className="city-cell" style={{ maxWidth: openCity === city.name ? "100%" : "30%", order: (index + 1) % 2 !== 0 ? 2 : 1, }} >
-                    
+                    <div className="accord-wrap">
                     <div className="accord" onClick={() => handleAccordionToggle(city.name)}
                           style={{ cursor: "pointer", fontWeight: "bold" }}>
-                    <h2>{city.name}</h2>
+                    <h2>{city.name}</h2> <div className="icon"> <img 
+      src={openCity === city.name ? '../src/assets/minus.svg' : '../src/assets/icon.svg'} 
+      alt="city"
+    /></div></div>
                     </div>
                     {openCity === city.name && (
-                          <div className="accordion-body" style={{ padding: "10px", background: "#f9f9f9",maxWidth:"100%", }}>
+                          <div className="accordion-body" style={{ padding: "10px",maxWidth:"100%", }}>
                             {/* Example details — you can show actual data */}
+                            <div className="accordian-wrapper">
                             <div className="tour-packages">
-                              <h3>{city.name} Tour Packages</h3>
+                              <h3>Tour Packages</h3>
                               <div className="all-tour-packages">
                                 {himanchal.data.packages.slice(0, limit).map((pack)=>{
                                     return <h4 key={pack.id}>{pack.name.substring(0, 35)}...</h4>
@@ -52,7 +56,29 @@ const CityList = ({ data }) => {
                               </div>
                             </div>
                             <div className="family-packages">
-                              <h3>{city.name} Family Packages</h3>
+                              <h3>Family Packages</h3>
+                              <div className="all-family-packages">
+                              {himanchal.data.packages.slice(0, limit).map((pack)=>{
+                                    return <h4 key={pack.id}>{pack.name.substring(0, 35)}...</h4>
+                                })}
+                                <div className="btn-show">
+                                {limit ? (<button onClick={() => setLimit(limit.length)}>Show More</button>) : (<button onClick={() => setLimit(9)}>Show less</button>)}
+                                </div>
+                              </div>
+                            </div>
+                            <div className="family-packages">
+                              <h3>Honeymoon Packages</h3>
+                              <div className="all-family-packages">
+                              {himanchal.data.packages.slice(0, limit).map((pack)=>{
+                                    return <h4 key={pack.id}>{pack.name.substring(0, 35)}...</h4>
+                                })}
+                                <div className="btn-show">
+                                {limit ? (<button onClick={() => setLimit(limit.length)}>Show More</button>) : (<button onClick={() => setLimit(9)}>Show less</button>)}
+                                </div>
+                              </div>
+                            </div>
+                            <div className="family-packages">
+                              <h3>Luxury Tour Packages</h3>
                               <div className="all-family-packages">
                               {himanchal.data.packages.slice(0, limit).map((pack)=>{
                                     return <h4 key={pack.id}>{pack.name.substring(0, 35)}...</h4>
@@ -63,9 +89,12 @@ const CityList = ({ data }) => {
                               </div>
                             </div>
                     
-
+                            </div>
                             {/* Link to city page */}
+                            <div className="pc-btn">
                             <Link to={`/state/${stateName}/${city.name}`}>Explore Packages</Link>
+                                {limit ? (<button onClick={() => setLimit(limit.length)}>Show More</button>) : (<button onClick={() => setLimit(9)}>Show less</button>)}
+                                </div>
                           </div>
                         )}
                     
